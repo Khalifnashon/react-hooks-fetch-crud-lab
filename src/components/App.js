d515b99407d4807c0b84b2ae313e97d5717de461
+ <+>
2023-01-23 23:46:17 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
+  // States
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  // Fetch requests
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((response) => response.json())
+    .then((questions) => setQuestions(questions))
+    }, []);
+
+  // Event Handlers
+  function handleAddNewQuestion(newQuestion){
+    // console.log(newQuestion);
+    
+    // Add the new question to the questions state
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDeleteQuestion(deletedQuestion){
+    // console.log("In App.js: ", deletedQuestion);
+    // Filter out the deleted question and update questions state
+    const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+    setQuestions(updatedQuestions);
+  }
+
+  function handleUpdateQuestion(updatedQuestion){
+    // console.log("In App.js: ", updatedQuestion);
+    const updatedQuestions = questions.map((question) => {
+      if(question.id !== updatedQuestion.id){
+        return question;
+      } else {
+        return updatedQuestion;
+      }
+
+    })
+    // console.log("Updated questions: ", updatedQuestions);
+    setQuestions(updatedQuestions);
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddNewQuestion={handleAddNewQuestion}/> : <QuestionList questions={questions} onDeleteQuestion={handleDeleteQuestion} onUpdateQuestion={handleUpdateQuestion}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  // Event Handlers
+  function handleDeleteClick(){
+    // console.log(question);
+
+    // DELETE Request
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then(() => onDeleteQuestion(question))
+  }
+
+  function handleAnswerChange(event){
+    // console.log(event.target.value);
+
+    // PATCH Request
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify({correctIndex: event.target.value}),
+    })
+    .then((response) => response.json())
+    .then((updatedQuestion) => onUpdateQuestion(updatedQuestion))
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +42,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
